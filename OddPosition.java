3bef8eaea43c2328cd7a6c4d92352f8f5e9fc93e
Pravin Novak <pravinnovak2707@gmail.com>
2022-11-15 23:24:36 +0530

    [Fifth Commit]added odd position

---
+++ OddPosition.java
@@ -0,0 +1,12 @@
+package jfk;
+
+public class OddPosition {
+    public static void main(String[] args) {
+        int[] arr =new int[]{2, 6, 9, 11, 50, 63};
+        System.out.println("Elements on array present on even position: ");
+        for (int i = 0; i < arr.length; i = i+2){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
