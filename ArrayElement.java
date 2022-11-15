354b32f3f076b6b30b7a89caced450ee99f86070
Pravin Novak <pravinnovak2707@gmail.com>
2022-11-15 19:36:53 +0530

    [First Commit] added array element

---
+++ ArrayElement.java
@@ -0,0 +1,11 @@
+package jfk;
+
+public class ArrayElement {
+    public static void main(String[] args) {
+        int[] array = {1 , 3, 5, 7, 9};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
